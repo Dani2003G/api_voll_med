8411e20254ce20e3bbdc46488a7b655ac7c40dac
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2025-02-21 21:40:48 -0300

    Adicao do controller para fazer o agendamento e cancelamento de consultas

---
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -0,0 +1,34 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import med.voll.api.domain.consulta.AgendaDeConsulta;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.consulta.DadosCancelamentoConsulta;
+import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/consultas")
+public class ConsultaController {
+
+    private final AgendaDeConsulta agenda;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<DadosDetalhamentoConsulta> agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
+        DadosDetalhamentoConsulta dto = agenda.agendar(dados);
+        return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping
+    @Transactional
+    public ResponseEntity cancelar(@RequestBody @Valid DadosCancelamentoConsulta dados) {
+        agenda.cancelar(dados);
+        return ResponseEntity.noContent().build();
+    }
+
+}
